b44d49b7e9040bee2cf37378fc97844a1f197fba
Miguel Torres <migueltg93@gmail.com>
2018-05-19 11:39:59 -0500

    Typo

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   it { should belong_to(:todo) }
-  it { shoul validate_prescense_of(:name) }
+  it { shoul validate_presence_of(:name) }
 end
