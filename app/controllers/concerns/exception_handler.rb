c7a641a7adf0825043a4f2572131a32bc4b11814
Miguel Torres <migueltg93@gmail.com>
2018-05-22 19:56:45 -0500

    JWT singleton, catch exceptions thrown with Exception Hanlder

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,14 +1,26 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches Standard Errors
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
   included do
+    # Define custom handlers
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExceptionHandler::MissintToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+
     rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
-
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
+      json_response({ message: e.message }, :not_found )
     end
   end
-end
+
+    private
+
+    def unauthorized_request(e)
+      json_response({ message: e.message }, :unauthorized)
+    end
+  end
