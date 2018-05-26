7743b2f14ca7a49133cadeb1a923d0b792097a2e
Miguel Torres <migueltg93@gmail.com>
2018-05-26 11:20:15 -0500

    authorize api request

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -10,7 +10,7 @@ module ExceptionHandler
     # Define custom handlers
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissintToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
 
     rescue_from ActiveRecord::RecordNotFound do |e|
